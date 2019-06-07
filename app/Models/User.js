76fb8f8b50fb5192ab4b6224ce2079711a7f250d
fahriansyah <fachryansyah123@gmail.com>
2019-06-07 09:28:08 +0700

    add relation

--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,10 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  memo () {
+    return this.hasMany("App/Models/Memo")
+  }
 }
 
 module.exports = User
